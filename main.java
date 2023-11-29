1e570d1b8754107497feeca67bfaaafbff061bb7
Rafa Ballesteros <i12bapar@uco.es>
2023-11-29 10:49:59 +0100

    actualizacion

---
+++ main.java
@@ -0,0 +1,11 @@
+package project;
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		usuario usuario1=new usuario(77036914,"RafaBalle","i12bapar@uco.es","123456789");
+		usuario1.mostrarInformacionUsuario();
+	}
+	}
+
+

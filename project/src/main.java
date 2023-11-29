9c161296d27e70e2352c133a87a0fc8ce1b0cda6
Rafael Ballesteros Padial <109797535+RafaBalle@users.noreply.github.com>
2023-11-29 10:25:32 +0100

    Delete project directory

--- project/src/main.java
+++
@@ -1,8 +0,0 @@
-package project;
-public class main {
-
-	public static void main(String[] args) {
-		usuario usuario1=new usuario(77036914,"Rafael Ballesteros","i12bapar@uco.es","asunguarra");
-		usuario1.mostrarInformacionUsuario();
-	}
-}
